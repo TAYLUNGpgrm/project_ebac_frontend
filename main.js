363e10c6751dc30701b4db5b6c30ee17c32d9e98
Alef Dutra <alefdutran@gmail.com>
2024-11-08 14:30:59 -0300

    Glow Up!

---
+++ main.js
@@ -0,0 +1,15 @@
+let previousScrollPosition = window.pageYOffset;
+
+window.onscroll = function() {
+    let currentScrollPosition = window.pageYOffset;
+    
+    if (previousScrollPosition < currentScrollPosition) {
+        // Rolando para baixo - esconde o cabeçalho
+        document.querySelector("header").style.transform = "translateY(-100%)"; // Move o header para fora da tela
+    } else {
+        // Rolando para cima - mostra o cabeçalho
+        document.querySelector("header").style.transform = "translateY(0)";
+    }
+    
+    previousScrollPosition = currentScrollPosition;
+}
