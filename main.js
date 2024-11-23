829d116721f1f88c5441099c6acc9838dc04fd47
Alef Dutra <alefdutran@gmail.com>
2024-11-23 14:06:38 -0300

    Indentação

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let previousScrollPosition = window.pageYOffset; // Posição inicial do scroll
 
-window.onscroll = function() {
+window.onscroll = function () {
     let currentScrollPosition = window.pageYOffset; // Posição do scroll atual
-    
+
     if (previousScrollPosition < currentScrollPosition) {
         // Rolando para baixo - esconde o cabeçalho
         document.querySelector("header").style.transform = "translateY(-100%)"; // Move o header para fora da tela
@@ -10,6 +10,6 @@ window.onscroll = function() {
         // Rolando para cima - mostra o cabeçalho
         document.querySelector("header").style.transform = "translateY(0)"; // Move o header de volta para o topo
     }
-    
+
     previousScrollPosition = currentScrollPosition; // Atualiza a posição anterior do scroll
 }
