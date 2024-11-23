17a3e4df5abc8800a1ca6553a1e14b9dccb4a12f
Alef Dutra <alefdutran@gmail.com>
2024-11-22 22:53:09 -0300

    Reendentação e melhorias na responsividade

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-let previousScrollPosition = window.pageYOffset;
+let previousScrollPosition = window.pageYOffset; // Posição inicial do scroll
 
 window.onscroll = function() {
-    let currentScrollPosition = window.pageYOffset;
+    let currentScrollPosition = window.pageYOffset; // Posição do scroll atual
     
     if (previousScrollPosition < currentScrollPosition) {
         // Rolando para baixo - esconde o cabeçalho
         document.querySelector("header").style.transform = "translateY(-100%)"; // Move o header para fora da tela
     } else {
         // Rolando para cima - mostra o cabeçalho
-        document.querySelector("header").style.transform = "translateY(0)";
+        document.querySelector("header").style.transform = "translateY(0)"; // Move o header de volta para o topo
     }
     
-    previousScrollPosition = currentScrollPosition;
+    previousScrollPosition = currentScrollPosition; // Atualiza a posição anterior do scroll
 }
-
-previous 
